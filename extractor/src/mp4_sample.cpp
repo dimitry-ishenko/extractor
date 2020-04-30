dff38b9d23e82e34dd9368b9825064cb5e250037
Dimitry Ishenko <dimitry@cos.flag.org>
2020-04-30 00:59:05 -0400

    mp4::sample: add data() and size()
    Make header-only.

--- extractor/src/mp4_sample.cpp
+++
@@ -1,6 +0,0 @@
-#include "mp4_sample.hpp"
-#include <utility>
-
-mp4::sample::sample(std::unique_ptr<AP4_DataBuffer> data) :
-    data_(std::move(data))
-{ }
--- extractor/src/mp4_sample.hpp
+++ extractor/src/mp4_sample.hpp
@@ -2,7 +2,9 @@
 #define MP4_SAMPLE_HPP
 
 #include "Ap4.h"
+
 #include <memory>
+#include <utility>
 
 namespace mp4
 {
@@ -13,11 +15,16 @@ public:
     sample() = default;
     bool is_valid() const { return !!data_; }
 
+    auto data() const { return data_ ? data_->GetData() : nullptr; }
+    auto size() const { return data_ ? data_->GetDataSize() : 0; }
+
 private:
     std::unique_ptr<AP4_DataBuffer> data_;
 
     friend class track;
-    explicit sample(std::unique_ptr<AP4_DataBuffer>);
+    explicit sample(std::unique_ptr<AP4_DataBuffer> data) :
+        data_(std::move(data))
+    { }
 };
 
 }
