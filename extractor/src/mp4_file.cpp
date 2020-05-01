950c97c949cef11006c2626bce7f2fd4183fe001
Dimitry Ishenko <dimitry@cos.flag.org>
2020-05-01 12:08:10 -0400

    mp4::file: strip off all garbage at end
    Clean-ups.

--- extractor/src/mp4_file.cpp
+++ extractor/src/mp4_file.cpp
@@ -3,20 +3,19 @@
 #include <iostream>
 #include <stdexcept>
 
-using std::cout;
-using std::endl;
-
 namespace mp4
 {
 
 constexpr auto meta = AP4_ATOM_TYPE('m', 'e', 't', 'a');
 constexpr auto mett = AP4_ATOM_TYPE('m', 'e', 't', 't');
 
+constexpr auto npos = std::string::npos;
+
 file::file(const std::string & name)
 {
     AP4_ByteStream* s;
 
-    cout << "Opening file: " << name << endl;
+    std::cout << "Opening file: " << name << std::endl;
     auto res = AP4_FileByteStream::Create(name.data(), AP4_FileByteStream::STREAM_MODE_READ, s);
     if(AP4_FAILED(res)) throw std::invalid_argument("Failed to open file");
 
@@ -28,39 +27,41 @@ track file::find_trans()
 {
     auto& tracks = file_->GetMovie()->GetTracks();
     auto count = tracks.ItemCount();
-    cout << "Found " << count << " tracks" << endl;
+    std::cout << "Found " << count << " tracks" << std::endl;
 
     for(decltype(count) n = 0; n < count; ++n)
     {
         AP4_Track* trk;
 
-        cout << "Processing track: " << n << endl;
+        std::cout << "Processing track: " << n << std::endl;
         auto res = tracks.Get(n, trk);
         if(AP4_FAILED(res)) throw std::invalid_argument("Failed to access track");
 
         if(trk->GetHandlerType() == meta)
         {
-            cout << "Detected metadata track: ID=" << trk->GetId() << " name=" << trk->GetTrackName() << endl;
+            std::cout << "Detected metadata track: ID=" << trk->GetId() << " name=" << trk->GetTrackName() << std::endl;
 
             auto table = trk->GetSampleTable();
             if(!table) throw std::invalid_argument("Missing sample table");
 
             auto count = table->GetSampleDescriptionCount();
-            cout << "Found " << count << " sample descriptions" << endl;
+            std::cout << "Found " << count << " sample descriptions" << std::endl;
 
             for(decltype(count) n = 0; n < count; ++n)
             {
-                cout << "Processing sample description: " << n << endl;
+                std::cout << "Processing sample description: " << n << std::endl;
                 auto desc = table->GetSampleDescription(n);
 
                 if(desc->GetFormat() == mett)
                 {
-                    cout << "Detected timed text format" << endl;
+                    std::cout << "Detected timed text format" << std::endl;
                     if(auto atom = dynamic_cast<AP4_UnknownSampleEntry*>(desc->ToAtom()))
                     {
                         auto payload = atom->GetPayload();
-                        std::string type(reinterpret_cast<const char*>(payload.GetData()), payload.GetDataSize());
-                        if(type.back() == 0) type.pop_back();
+                        std::string type { payload.GetData(), payload.GetData() + payload.GetDataSize() };
+
+                        auto pos = type.find('\0');
+                        if(pos != npos) type.resize(pos);
 
                         // Bento4 eats the first 8 bytes of "application/transcription_1"
                         // thinking they are Reserved and Data reference index fields (6+2)
@@ -68,7 +69,7 @@ track file::find_trans()
 
                         if(type == "ion/transcription_1")
                         {
-                            cout << "Detected application/transcription_1 MIME type" << endl;
+                            std::cout << "Detected application/transcription_1 MIME type" << std::endl;
                             return track(trk);
                         }
                     }
@@ -76,7 +77,7 @@ track file::find_trans()
             }
         }
 
-        cout << "Skipping" << endl;
+        std::cout << "Skipping" << std::endl;
     }
 
     return track();
